b3573c3265c4baa0fd28964e829d6f5dbd4d8a05
Yuta OGIHARA <ogi@zsrv.net>
2012-04-20 20:09:30 +0900

    initial commit

---
+++ meteor-chat.js
@@ -0,0 +1,40 @@
+Messages = new Meteor.Collection("messages");
+
+if (Meteor.is_client) {
+
+    Template.signin.signined_name = function () {
+        return Session.get('signined_name')
+    };
+
+    Template.signin.events = {
+        'click button.btn': function () {
+            Session.set('signined_name', $('#name').val());
+        }
+    };
+
+    Template.herounit.signined_name = function () {
+        return Session.get('signined_name')
+    };
+
+
+    Template.herounit.events = {
+        'keydown input.#message': function (e) {
+            if(e.keyCode == 13) {
+                Messages.insert({name:Session.get('signined_name'), message:$('#message').val(), at: new Date()});
+                $('#message').val('');
+            }
+        }
+    };
+
+
+    Template.content.messages = function () {
+        return Messages.find({}, {sort:{at: -1}});
+    };
+
+}
+
+if (Meteor.is_server) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
